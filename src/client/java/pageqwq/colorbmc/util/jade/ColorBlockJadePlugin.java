becbea36be5aa66636c6741aaf873645d3214c56
WaterFrog <waterfrog250@outlook.com>
2026-08-09 11:25:39 +0800

    feat: add Jade integration showing hex color, swatch and tinted icon, v2.7.4

---
+++ src/client/java/pageqwq/colorbmc/util/jade/ColorBlockJadePlugin.java
@@ -0,0 +1,24 @@
+package pageqwq.colorbmc.util.jade;
+
+import net.minecraft.resources.Identifier;
+import pageqwq.colorbmc.block.RGBBlock;
+import pageqwq.colorbmc.block.entity.RGBBlockEntity;
+import snownee.jade.api.BlockAccessor;
+import snownee.jade.api.IWailaClientRegistration;
+import snownee.jade.api.IWailaPlugin;
+import snownee.jade.api.WailaPlugin;
+
+@WailaPlugin
+public class ColorBlockJadePlugin implements IWailaPlugin {
+    public static final Identifier UID = Identifier.fromNamespaceAndPath("colorblockmc", "rgb_block");
+
+    @Override
+    public void registerClient(IWailaClientRegistration registration) {
+        registration.registerBlockIcon(RGBBlockIconProvider.INSTANCE, RGBBlock.class);
+        registration.registerBlockComponent(RGBBlockInfoProvider.INSTANCE, RGBBlock.class);
+    }
+
+    static int getColor(BlockAccessor accessor) {
+        return accessor.getBlockEntity() instanceof RGBBlockEntity rgb ? rgb.getColor() : -1;
+    }
+}
---
+++ src/client/java/pageqwq/colorbmc/util/jade/RGBBlockIconProvider.java
@@ -0,0 +1,39 @@
+package pageqwq.colorbmc.util.jade;
+
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.DyedItemColor;
+import org.jspecify.annotations.Nullable;
+import pageqwq.colorbmc.util.registries.DataComponentRegistry;
+import snownee.jade.api.BlockAccessor;
+import snownee.jade.api.IBlockComponentProvider;
+import snownee.jade.api.ITooltip;
+import snownee.jade.api.config.IPluginConfig;
+import snownee.jade.api.ui.Element;
+import snownee.jade.api.ui.JadeUI;
+
+public class RGBBlockIconProvider implements IBlockComponentProvider {
+    public static final RGBBlockIconProvider INSTANCE = new RGBBlockIconProvider();
+
+    @Override
+    public Identifier getUid() {
+        return ColorBlockJadePlugin.UID;
+    }
+
+    @Override
+    public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
+    }
+
+    @Override
+    public @Nullable Element getIcon(BlockAccessor accessor, IPluginConfig config, @Nullable Element currentIcon) {
+        int color = ColorBlockJadePlugin.getColor(accessor);
+        if (color == -1) {
+            return null;
+        }
+        ItemStack stack = new ItemStack(accessor.getBlock());
+        stack.set(DataComponentRegistry.COLOR, color);
+        stack.set(DataComponents.DYED_COLOR, new DyedItemColor(color));
+        return JadeUI.item(stack);
+    }
+}
